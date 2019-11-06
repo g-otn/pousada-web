35f68e28df54f4d9eba25f30800cd99fb58da964
g-otn <g_otn@hotmail.com>
2019-11-06 18:17:00 -0300

    create DAO implementations initial classes

---
+++ src/java/br/com/gotn/pousada/dao/impl/QuartoDAO.java
@@ -0,0 +1,29 @@
+package br.com.gotn.pousada.dao.impl;
+
+import br.com.gotn.pousada.dao.IDAO;
+import br.com.gotn.pousada.dominio.EntidadeDominio;
+import java.util.List;
+
+public class QuartoDAO implements IDAO {
+
+    @Override
+    public void salvar(EntidadeDominio entidadeDominio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterar(EntidadeDominio entidadeDominio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void excluir(EntidadeDominio entidadeDominio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<EntidadeDominio> consultar(EntidadeDominio entidadeDominio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
