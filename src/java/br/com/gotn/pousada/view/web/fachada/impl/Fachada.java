2e6adfd47d45b795ceaeaae1ab5164c22fdb9bfb
g-otn <g-otn@hotmail.com>
2019-11-13 01:57:28 -0300

    create initial facade

---
+++ src/java/br/com/gotn/pousada/view/web/fachada/impl/Fachada.java
@@ -0,0 +1,49 @@
+package br.com.gotn.pousada.view.web.fachada.impl;
+
+import br.com.gotn.pousada.dominio.EntidadeDominio;
+import br.com.gotn.pousada.dominio.Resultado;
+import br.com.gotn.pousada.negocio.IStrategy;
+import br.com.gotn.pousada.view.web.fachada.IFachada;
+import java.util.List;
+import java.util.Map;
+
+public class Fachada implements IFachada {
+    
+    private Map<String, Map<String, List<IStrategy>>> regrasNegocio;
+    private StringBuilder sb = new StringBuilder();
+    private Resultado resultado;
+
+    public Fachada() {
+
+    }
+
+    @Override
+    public Resultado salvar(EntidadeDominio entidade) {
+        System.out.println("--> Fachada#salvar");
+    }
+
+    @Override
+    public Resultado alterar(EntidadeDominio entidade) {
+        System.out.println("--> Fachada#alterar");
+    }
+
+    @Override
+    public Resultado excluir(EntidadeDominio entidade) {
+        System.out.println("--> Fachada#excluir");
+    }
+
+    @Override
+    public Resultado consultar(EntidadeDominio entidade) {
+        System.out.println("--> Fachada#consultar");
+    }
+
+    private void executarRegras(EntidadeDominio entidade, List<IStrategy> rnsEntidade) {
+        for (IStrategy rn : rnsEntidade) {
+            String msg = rn.processar(entidade);
+            if (msg != null) {
+                sb.append(msg);
+            }
+        }
+    }
+
+}
