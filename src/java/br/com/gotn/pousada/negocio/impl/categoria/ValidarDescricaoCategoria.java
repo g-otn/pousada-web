61907164118e443f06a1160487afec20e2c0bf80
g-otn <g_otn@hotmail.com>
2019-11-14 19:44:46 -0300

    create business validations for Categoria

--- src/java/br/com/gotn/pousada/negocio/impl/categoria/ValidarDescricaoCategoria.java
+++ src/java/br/com/gotn/pousada/negocio/impl/categoria/ValidarDescricaoCategoria.java
@@ -4,7 +4,7 @@ import br.com.gotn.pousada.dominio.Categoria;
 import br.com.gotn.pousada.dominio.EntidadeDominio;
 import br.com.gotn.pousada.negocio.IStrategy;
 
-public class ValidarDadosCategoria implements IStrategy {
+public class ValidarDescricaoCategoria implements IStrategy {
 
     @Override
     public String processar(EntidadeDominio entidade) {
@@ -12,12 +12,10 @@ public class ValidarDadosCategoria implements IStrategy {
         Categoria categoria = (Categoria) entidade;
         StringBuilder sb = new StringBuilder();
         
-        if (categoria.getCapacidade() < 1) {
-            sb.append("capacidade: A capacidade é inválida\n");
-        }
-        
-        if (categoria.getPrecoDiaria() < 0.0) {
-            sb.append("precoDiaria: O preço da diária é inválido");
+        if (categoria.getDescricao() == null || categoria.getDescricao().trim().isEmpty()) {
+            sb.append("descricao: A descrição é obrigatória");
+        } else {
+            // TODO: Verificar existência com DAO
         }
         
         return sb.toString();
