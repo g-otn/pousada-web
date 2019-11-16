67b1a4f39b3e0c6c8243dd36d4b5df0592a3b08c
g-otn <g-otn@hotmail.com>
2019-11-16 00:58:23 -0300

    create erros.js

---
+++ web/js/erros.js
@@ -0,0 +1,30 @@
+// Mapeia as mensagens de erros e as exibe nos campos devidos
+// Formato esperado: "idInput: Mensagem de Erro\nidInput2: Mensagem de Erro2\n ..."
+function exibirErros(data) {
+    if (!data || !data.trim()) return
+
+    $(document).ready(() => {
+
+        data = data.split('\n')
+    
+        console.log(`Exibindo ${data.length} mensagens de erro`)
+        const erros = data.map(linha => {
+            if (!linha || !linha.trim()) return
+
+            linha = linha.split(':')
+            return {
+                elementoCampo: `#${linha[0]}`,
+                elementoMensagemErro: `#${linha[0]}Erro`,
+                mensagem: linha[1].trim()
+            }
+        })
+
+        console.log('Erros mapeados: ', erros, '\naplicando-os ao DOM')
+
+        erros.forEach(erro => {
+            $(erro.elementoCampo).addClass('is-invalid')
+            $(erro.elementoMensagemErro).text(erro.mensagem)
+        });
+    
+    })
+}
