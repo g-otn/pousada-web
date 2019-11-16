2a44e35754e4457862d4851498610752f26f1596
g-otn <g-otn@hotmail.com>
2019-11-16 18:47:00 -0300

    create initial viewHelpers

---
+++ src/java/br/com/gotn/pousada/view/web/viewhelper/impl/CategoriaVH.java
@@ -0,0 +1,21 @@
+package br.com.gotn.pousada.view.web.viewhelper.impl;
+
+import br.com.gotn.pousada.dominio.EntidadeDominio;
+import br.com.gotn.pousada.dominio.Resultado;
+import br.com.gotn.pousada.view.web.viewhelper.IViewHelper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CategoriaVH implements IViewHelper {
+
+    @Override
+    public EntidadeDominio getEntidade(HttpServletRequest request) {
+        System.out.println("--> CategoriaVH#getEntidade");
+    }
+
+    @Override
+    public void setView(Resultado resultado, HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("--> CategoriaVH#setView");
+    }
+    
+}
