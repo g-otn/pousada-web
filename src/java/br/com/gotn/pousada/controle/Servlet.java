a3bfdd9de4eaf2c79fd6fc9e498eb782c756c640
g-otn <g-otn@hotmail.com>
2019-11-19 02:05:41 -0300

    add more logging

--- src/java/br/com/gotn/pousada/controle/Servlet.java
+++ src/java/br/com/gotn/pousada/controle/Servlet.java
@@ -49,11 +49,16 @@ public class Servlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        System.out.println("\n=================================");
+        
         // Retorna o caminho sem a parte da aplicação ou da servlet (http://localhost:8080/ReservasPousada/Servlet/reservas -> /reservas)
         String caminho = request.getPathInfo(); 
 
         // Retorna a operação desejada (salvar, excluir, alterar ou consultar)
         String operacao = request.getParameter("operacao");
+        
+        System.out.println("==== Nova requisição POST para " + caminho);
+        System.out.println("==== Operação: " + operacao);
         
         // *****************************************************************************************
         
@@ -63,11 +68,15 @@ public class Servlet extends HttpServlet {
         // Caminho não maepado (não encontrado em viewHelperMap)
         if (viewHelper == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND); // HTTP 404
+            System.out.println("==== viewHelper nao encontrado para o caminho " + caminho);
+            System.out.println("=================================\n");
             return;
         }
+        System.out.println("==== viewHelper recuperado: " + viewHelper.getClass().getSimpleName());
 
         // Retorna a entidade instanciada e preenchida com os dados enviados na requisição
         EntidadeDominio entidade = viewHelper.getEntidade(request);
+        System.out.println("==== entidade recuperada: " + entidade.getClass().getSimpleName());
 
         // Retorna a command designada para executar a operação
         ICommand command = commandMap.get(operacao);
@@ -75,18 +84,22 @@ public class Servlet extends HttpServlet {
         // Operação não suportada (não encontrada em commandMap)
         if (command == null) {
             response.setStatus(422); // HTTP 422: Entidade improcessável (Sintaxe correta porém dados inválidos (operação inválida))
+            System.out.println("==== command nao encontrado para o caminho " + caminho);
+            System.out.println("=================================\n");
             return;
         }
+        System.out.println("==== command recuperado: " + command.getClass().getSimpleName());
         
         // *****************************************************************************************
         
         // Retorna o resultado da entidade que foi executada e processada
         Resultado resultado = command.executar(entidade);
-
-        // *****************************************************************************************
+        System.out.println("==== Resultado: " + resultado.getEntidades().size() + " entidades, " + resultado.getMensagens().split("\n").length + " mensagens" );
         
         // Envia o resultado para a viewHelper que continuará o tratamento da requisição e enviará uma resposta
         viewHelper.setView(resultado, request, response);
+        
+        System.out.println("=================================\n");
     }
 
 }
